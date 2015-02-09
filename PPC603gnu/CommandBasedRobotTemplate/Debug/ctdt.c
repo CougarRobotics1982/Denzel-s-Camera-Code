a377b12b60143d91b03e4c50a9440fc2f913df74
CougarRobotics1982 <cougarrobotics1982@gmail.com>
2015-02-09 16:59:52 -0600

    Added Camera, Everything

---
+++ PPC603gnu/CommandBasedRobotTemplate/Debug/ctdt.c
@@ -0,0 +1,407 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__I__ZN8Command1C2Ev();
+
+void _GLOBAL__I__ZN10ShooterOn1C2Ev();
+
+void _GLOBAL__I__ZN10aLeft_BackC2Ev();
+
+void _GLOBAL__I__ZN10aLeft_SideC2Ev();
+
+void _GLOBAL__I__ZN11aRight_BackC2Ev();
+
+void _GLOBAL__I__ZN11aRight_SideC2Ev();
+
+void _GLOBAL__I__ZN12dCameraDriveC2Ev();
+
+void _GLOBAL__I__ZN12dDriveArcadeC2Ev();
+
+void _GLOBAL__I__ZN13dDriveMecanumC2Ev();
+
+void _GLOBAL__I__ZN10dDriveStopC2Ev();
+
+void _GLOBAL__I__ZN13dInvertMotorsC2Ev();
+
+void _GLOBAL__I__ZN8fPreset1C2Ev();
+
+void _GLOBAL__I__ZN8fPreset2C2Ev();
+
+void _GLOBAL__I__ZN8fPreset3C2Ev();
+
+void _GLOBAL__I__ZN8fPreset4C2Ev();
+
+void _GLOBAL__I__ZN6fShootC2Ev();
+
+void _GLOBAL__I__ZN10fShootBackC2Ev();
+
+void _GLOBAL__I__ZN12fShooterDownC2Ev();
+
+void _GLOBAL__I__ZN13fShooterNoPIDC2Ev();
+
+void _GLOBAL__I__ZN10fShooterOnC2Ev();
+
+void _GLOBAL__I__ZN10fShooterUpC2Ev();
+
+void _GLOBAL__I__ZN4nFanC2Ev();
+
+void _GLOBAL__I__ZN6nFanOnC2Ev();
+
+void _GLOBAL__I__ZN10nNetUpDownC2Ev();
+
+void _GLOBAL__I__ZN5pTiltC2Ev();
+
+void _GLOBAL__I__ZN6pTilt1C2Ev();
+
+void _GLOBAL__I__ZN9pTiltDownC2Ev();
+
+void _GLOBAL__I__ZN7pTiltUpC2Ev();
+
+void _GLOBAL__I__ZN8sAccelOnC2Ev();
+
+void _GLOBAL__I__ZN7sGyroOnC2Ev();
+
+void _GLOBAL__I__ZN2OIC2Ev();
+
+void _GLOBAL__I__ZN5Robot10driveTrainE();
+
+void _GLOBAL__I__ZN8RobotMap19driveTrainLeftFrontE();
+
+void _GLOBAL__I__ZN10AutonomousC2Ev();
+
+void _GLOBAL__I__ZN6CameraC2Ev();
+
+void _GLOBAL__I__ZN10DriveTrainC2Ev();
+
+void _GLOBAL__I__ZN3NetC2Ev();
+
+void _GLOBAL__I__ZN8PlatformC2Ev();
+
+void _GLOBAL__I__ZN7SensorsC2Ev();
+
+void _GLOBAL__I__ZN7ShooterC2Ev();
+
+void _GLOBAL__I__ZN8Shooter1C2Ev();
+
+void _GLOBAL__I__ZN12PrintCommandC2EPKc();
+
+void _GLOBAL__I__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__I__ZN11WaitCommandC2Ed();
+
+void _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__I__ZN10LiveWindow10m_instanceE();
+
+void _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__I__ZN8Resource12m_createLockE();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__I__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__I_AxisCamera_debugFlag();
+
+void _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__I__ZN16NetworkTableMode6ServerE();
+
+void _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__I_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE();
+
+void _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__I__ZN8Command1C2Ev,
+    _GLOBAL__I__ZN10ShooterOn1C2Ev,
+    _GLOBAL__I__ZN10aLeft_BackC2Ev,
+    _GLOBAL__I__ZN10aLeft_SideC2Ev,
+    _GLOBAL__I__ZN11aRight_BackC2Ev,
+    _GLOBAL__I__ZN11aRight_SideC2Ev,
+    _GLOBAL__I__ZN12dCameraDriveC2Ev,
+    _GLOBAL__I__ZN12dDriveArcadeC2Ev,
+    _GLOBAL__I__ZN13dDriveMecanumC2Ev,
+    _GLOBAL__I__ZN10dDriveStopC2Ev,
+    _GLOBAL__I__ZN13dInvertMotorsC2Ev,
+    _GLOBAL__I__ZN8fPreset1C2Ev,
+    _GLOBAL__I__ZN8fPreset2C2Ev,
+    _GLOBAL__I__ZN8fPreset3C2Ev,
+    _GLOBAL__I__ZN8fPreset4C2Ev,
+    _GLOBAL__I__ZN6fShootC2Ev,
+    _GLOBAL__I__ZN10fShootBackC2Ev,
+    _GLOBAL__I__ZN12fShooterDownC2Ev,
+    _GLOBAL__I__ZN13fShooterNoPIDC2Ev,
+    _GLOBAL__I__ZN10fShooterOnC2Ev,
+    _GLOBAL__I__ZN10fShooterUpC2Ev,
+    _GLOBAL__I__ZN4nFanC2Ev,
+    _GLOBAL__I__ZN6nFanOnC2Ev,
+    _GLOBAL__I__ZN10nNetUpDownC2Ev,
+    _GLOBAL__I__ZN5pTiltC2Ev,
+    _GLOBAL__I__ZN6pTilt1C2Ev,
+    _GLOBAL__I__ZN9pTiltDownC2Ev,
+    _GLOBAL__I__ZN7pTiltUpC2Ev,
+    _GLOBAL__I__ZN8sAccelOnC2Ev,
+    _GLOBAL__I__ZN7sGyroOnC2Ev,
+    _GLOBAL__I__ZN2OIC2Ev,
+    _GLOBAL__I__ZN5Robot10driveTrainE,
+    _GLOBAL__I__ZN8RobotMap19driveTrainLeftFrontE,
+    _GLOBAL__I__ZN10AutonomousC2Ev,
+    _GLOBAL__I__ZN6CameraC2Ev,
+    _GLOBAL__I__ZN10DriveTrainC2Ev,
+    _GLOBAL__I__ZN3NetC2Ev,
+    _GLOBAL__I__ZN8PlatformC2Ev,
+    _GLOBAL__I__ZN7SensorsC2Ev,
+    _GLOBAL__I__ZN7ShooterC2Ev,
+    _GLOBAL__I__ZN8Shooter1C2Ev,
+    _GLOBAL__I__ZN12PrintCommandC2EPKc,
+    _GLOBAL__I__ZN9Scheduler9_instanceE,
+    _GLOBAL__I__ZN11WaitCommandC2Ed,
+    _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__I__ZN10LiveWindow10m_instanceE,
+    _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__I__ZN8Resource12m_createLockE,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__I__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__I_AxisCamera_debugFlag,
+    _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__I__ZN16NetworkTableMode6ServerE,
+    _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__I_OutgoingEntryReceiver_NULL,
+    _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE,
+    _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
+
+void _GLOBAL__D__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__D__ZN8Command1C2Ev();
+
+void _GLOBAL__D__ZN10ShooterOn1C2Ev();
+
+void _GLOBAL__D__ZN10aLeft_BackC2Ev();
+
+void _GLOBAL__D__ZN10aLeft_SideC2Ev();
+
+void _GLOBAL__D__ZN11aRight_BackC2Ev();
+
+void _GLOBAL__D__ZN11aRight_SideC2Ev();
+
+void _GLOBAL__D__ZN12dCameraDriveC2Ev();
+
+void _GLOBAL__D__ZN12dDriveArcadeC2Ev();
+
+void _GLOBAL__D__ZN13dDriveMecanumC2Ev();
+
+void _GLOBAL__D__ZN10dDriveStopC2Ev();
+
+void _GLOBAL__D__ZN13dInvertMotorsC2Ev();
+
+void _GLOBAL__D__ZN8fPreset1C2Ev();
+
+void _GLOBAL__D__ZN8fPreset2C2Ev();
+
+void _GLOBAL__D__ZN8fPreset3C2Ev();
+
+void _GLOBAL__D__ZN8fPreset4C2Ev();
+
+void _GLOBAL__D__ZN6fShootC2Ev();
+
+void _GLOBAL__D__ZN10fShootBackC2Ev();
+
+void _GLOBAL__D__ZN12fShooterDownC2Ev();
+
+void _GLOBAL__D__ZN13fShooterNoPIDC2Ev();
+
+void _GLOBAL__D__ZN10fShooterOnC2Ev();
+
+void _GLOBAL__D__ZN10fShooterUpC2Ev();
+
+void _GLOBAL__D__ZN4nFanC2Ev();
+
+void _GLOBAL__D__ZN6nFanOnC2Ev();
+
+void _GLOBAL__D__ZN10nNetUpDownC2Ev();
+
+void _GLOBAL__D__ZN5pTiltC2Ev();
+
+void _GLOBAL__D__ZN6pTilt1C2Ev();
+
+void _GLOBAL__D__ZN9pTiltDownC2Ev();
+
+void _GLOBAL__D__ZN7pTiltUpC2Ev();
+
+void _GLOBAL__D__ZN8sAccelOnC2Ev();
+
+void _GLOBAL__D__ZN7sGyroOnC2Ev();
+
+void _GLOBAL__D__ZN2OIC2Ev();
+
+void _GLOBAL__D__ZN5Robot10driveTrainE();
+
+void _GLOBAL__D__ZN8RobotMap19driveTrainLeftFrontE();
+
+void _GLOBAL__D__ZN10AutonomousC2Ev();
+
+void _GLOBAL__D__ZN6CameraC2Ev();
+
+void _GLOBAL__D__ZN10DriveTrainC2Ev();
+
+void _GLOBAL__D__ZN3NetC2Ev();
+
+void _GLOBAL__D__ZN8PlatformC2Ev();
+
+void _GLOBAL__D__ZN7SensorsC2Ev();
+
+void _GLOBAL__D__ZN7ShooterC2Ev();
+
+void _GLOBAL__D__ZN8Shooter1C2Ev();
+
+void _GLOBAL__D__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__D__ZN8Resource12m_createLockE();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__D__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__D_AxisCamera_debugFlag();
+
+void _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__D_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__D__ZN8Command1C2Ev,
+    _GLOBAL__D__ZN10ShooterOn1C2Ev,
+    _GLOBAL__D__ZN10aLeft_BackC2Ev,
+    _GLOBAL__D__ZN10aLeft_SideC2Ev,
+    _GLOBAL__D__ZN11aRight_BackC2Ev,
+    _GLOBAL__D__ZN11aRight_SideC2Ev,
+    _GLOBAL__D__ZN12dCameraDriveC2Ev,
+    _GLOBAL__D__ZN12dDriveArcadeC2Ev,
+    _GLOBAL__D__ZN13dDriveMecanumC2Ev,
+    _GLOBAL__D__ZN10dDriveStopC2Ev,
+    _GLOBAL__D__ZN13dInvertMotorsC2Ev,
+    _GLOBAL__D__ZN8fPreset1C2Ev,
+    _GLOBAL__D__ZN8fPreset2C2Ev,
+    _GLOBAL__D__ZN8fPreset3C2Ev,
+    _GLOBAL__D__ZN8fPreset4C2Ev,
+    _GLOBAL__D__ZN6fShootC2Ev,
+    _GLOBAL__D__ZN10fShootBackC2Ev,
+    _GLOBAL__D__ZN12fShooterDownC2Ev,
+    _GLOBAL__D__ZN13fShooterNoPIDC2Ev,
+    _GLOBAL__D__ZN10fShooterOnC2Ev,
+    _GLOBAL__D__ZN10fShooterUpC2Ev,
+    _GLOBAL__D__ZN4nFanC2Ev,
+    _GLOBAL__D__ZN6nFanOnC2Ev,
+    _GLOBAL__D__ZN10nNetUpDownC2Ev,
+    _GLOBAL__D__ZN5pTiltC2Ev,
+    _GLOBAL__D__ZN6pTilt1C2Ev,
+    _GLOBAL__D__ZN9pTiltDownC2Ev,
+    _GLOBAL__D__ZN7pTiltUpC2Ev,
+    _GLOBAL__D__ZN8sAccelOnC2Ev,
+    _GLOBAL__D__ZN7sGyroOnC2Ev,
+    _GLOBAL__D__ZN2OIC2Ev,
+    _GLOBAL__D__ZN5Robot10driveTrainE,
+    _GLOBAL__D__ZN8RobotMap19driveTrainLeftFrontE,
+    _GLOBAL__D__ZN10AutonomousC2Ev,
+    _GLOBAL__D__ZN6CameraC2Ev,
+    _GLOBAL__D__ZN10DriveTrainC2Ev,
+    _GLOBAL__D__ZN3NetC2Ev,
+    _GLOBAL__D__ZN8PlatformC2Ev,
+    _GLOBAL__D__ZN7SensorsC2Ev,
+    _GLOBAL__D__ZN7ShooterC2Ev,
+    _GLOBAL__D__ZN8Shooter1C2Ev,
+    _GLOBAL__D__ZN9Scheduler9_instanceE,
+    _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__D__ZN8Resource12m_createLockE,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__D__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__D_AxisCamera_debugFlag,
+    _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__D_OutgoingEntryReceiver_NULL,
+    _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
